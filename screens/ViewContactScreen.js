e3187a63751dfb12f1ae3c99795d7060f2ce69be
Saad Patel <patelsaadn@gmail.com>
2019-07-11 16:01:02 +0530

    commit6

--- screens/ViewContactScreen.js
+++ screens/ViewContactScreen.js
@@ -23,30 +23,123 @@ export default class ViewContactScreen extends React.Component{
 
   componentDidMount(){
     const { navigation } = this.props;
-  navigation.addListener("WillFocus", ()=> {
+    // will != Will
+
+  navigation.addListener("willFocus", ()=> {
+    // Param !=Params
     var key = this.props.navigation.getParam("key","")
+    // Below linewill import everything..
+    this.getContact(key)
     
   })
   
 }
 
-getContact =  async key => {  
-  await AsyncStorage.getItem(key)
-  .then(contactjsonstring => {
-    var contact = JSON.parse(contactjsonstring)
-    contact[key] = key
-    this.setState(contact)
-  })
-  .catch(error =>{
-    console.log(error)
-  })
-}
+  getContact =  async key => {  
+    await AsyncStorage.getItem(key)
+    .then(contactjsonstring => {
+      var contact = JSON.parse(contactjsonstring)
+      contact["key"] = key
+      this.setState(contact)
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+  }
+
+
+  
+  callAction = phone =>{
+    let phoneNumber = phone;
+    if (Platform.OS !== "android") {
+      phoneNumber = `telpromt: ${phone}`
+    } else {
+      phoneNumber = `tel:${phone}`
+    }
+
+    Linking.canOpenURL(phoneNumber)
+    .then( supported => {
+      if (!supported) {
+        Alert.alert("Phone number is not available")
+      } else {
+        return Linking.openURL(phoneNumber)
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  smsAction = phone =>{
+    let phoneNumber = phone;
+    phoneNumber= `sms:${phone}`
+
+
+    Linking.canOpenURL(phoneNumber)
+    .then( supported => {
+      if (!supported) {
+        Alert.alert("Phone number is not available")
+      } else {
+        return Linking.openURL(phoneNumber)
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  editContact = (key) => {
+    this.props.navigation.navigate("Edit", {key:key})
+  }
 
 render(){
   return (
-    <View style={styles.container}>
-      <Text>view</Text>
+    <ScrollView style={styles.container}>
+
+      <View style={styles.contactIconContainer}>
+        <Text style={styles.contactIcon }>
+          {this.state.fname[0].toUpperCase()}
+        </Text>
+        <View style={styles.nameContainer}>
+        <Text style={styles.name}>
+          {this.state.fname} {this.state.lname}
+        </Text>
+      </View>
     </View>
+
+      
+
+    <View style={styles.infoContainer}>
+      
+      <Card>
+        <CardItem bordered> 
+          <Text style={styles.infoText}>Phone</Text>
+        </CardItem>
+        <CardItem>
+          <Text style={styles.infoText}>{this.state.phone}</Text>
+        </CardItem>
+      </Card>
+
+      <Card>
+        <CardItem bordered> 
+          <Text style={styles.infoText}>Email</Text>
+        </CardItem>
+        <CardItem>
+          <Text style={styles.infoText}>{this.state.email}</Text>
+        </CardItem>
+      </Card>
+
+      <Card>
+        <CardItem bordered> 
+          <Text style={styles.infoText}>Address</Text>
+        </CardItem>
+        <CardItem>
+          <Text style={styles.infoText}>{this.state.address}</Text>
+        </CardItem>
+      </Card>
+
+    </View>
+    </ScrollView>
   );
 }
 }
@@ -55,8 +148,50 @@ render(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
+  },
+  contactIconContainer: {
+    height: 200,
+    backgroundColor: "#B83227",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  contactIcon: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  nameContainer: {
+    width: "100%",
+    height: 70,
+    padding: 10,
+    backgroundColor: "rgba(255,255,255,0.5)",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0
+  },
+  name: {
+    fontSize: 24,
+    color: "#000",
+    fontWeight: "900"
+  },
+  infoText: {
+    fontSize: 18,
+    fontWeight: "300"
+  },
+  actionContainer: {
+    flexDirection: "row"
+  },
+  actionButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  actionText: {
+    color: "#B83227",
+    fontWeight: "900"
+  },
+  infoContainer: {
+    flexDirection: "column"
   },
 });
